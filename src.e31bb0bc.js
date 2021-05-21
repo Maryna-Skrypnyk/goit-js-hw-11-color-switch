28227eb94640ddc71ed83b11c1bfd1df8b0c5a4b
Maryna-Skrypnyk <marina.skrypnyk@gmail.com>
2021-05-21 13:00:10 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -220,6 +220,7 @@ function onBtnStartClick(e) {
   stopBtn.disabled = false;
   colorIntervalId = setInterval(function (min, max) {
     document.body.style.backgroundColor = _colorsData.default[randomIntegerFromInterval(min, max)];
+    localStorage.setItem('color', document.body.style.backgroundColor);
   }, 1000, 0, _colorsData.default.length - 1);
 }
 
@@ -288,7 +289,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53449" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55050" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
